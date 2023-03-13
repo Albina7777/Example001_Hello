24d7c7617655b761ca96998a739231c0dab02008
Albina7777 <al-spiritus@mail.ru>
2023-03-13 14:28:44 +0300

    add if

---
+++ Example005_IfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "гарри")//чтобы не было проблем с регистром, ToLower
+{
+  Console.WriteLine("Ура, это же ГАРРИ!!!");
+}
+else
+{
+  Console.Write("Привет, ");
+  Console.WriteLine(username);
+}
